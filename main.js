2e9305ba34a918fb0c0b627fd4abdfdd655fdf86
Rahish2009 <57913293+Rahish2009@users.noreply.github.com>
2021-06-17 16:22:39 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_qualaity:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach(  '#camera'  );
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log("ml5_version", ml5_version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZsNeuU-ph/9',modelLoaded);
+function modelLoaded()
+{
+console.log('Model Loaded!');
+}
